4408ecb29484ae14f5eeacc00145eb6fb1d9f7ed
Daniel Ayala <dmayala001@gmail.com>
2015-08-14 19:25:24 -0700

    Update bootstrap requires

--- app/components/MainApp.react.js
+++ app/components/MainApp.react.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {RouteHandler} from 'react-router';
 
+if (process.env.BROWSER) {
+  require('stylesheets/app');
+}
+
 export class MainApp extends React.Component {
   render() {
     const props = Object.assign({}, this.state, this.props);
