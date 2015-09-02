0cfa3e26f8881c07ab1f327bc40dc9cdb6cac61b
Daniel Ayala <dmayala001@gmail.com>
2015-09-01 20:01:06 -0700

    Add endpoint for creating carts

--- server/routes/carts.js
+++ server/routes/carts.js
@@ -1,22 +1,26 @@
 import express from 'express';
+import Cart from 'models/cart';
 import CartCollection from 'collections/carts';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   let expansions = req.query.expand;
-
-  // let relations = {
-  //   'creator': 'creator',
-  //   'choices': 'choices',
-  //   'votes': 'votes',
-  //   'users': 'votes.user'
-  // };
-
   let withRelated = expansions ? expansions.split(',') : [];
 
   let carts = await CartCollection.forge().fetch({ withRelated });
   res.send(carts);
+});
+
+
+router.post('/', async (req, res) => {
+  try {
+    let cart = await Cart.forge().save();
+    res.send(cart);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ 'error': 'An error has occurred' });
+  }
 });
 
 module.exports = router;
